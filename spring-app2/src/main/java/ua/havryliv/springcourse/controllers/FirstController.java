a99e8193b7da0f12c0bdd57175d3fac1fda833a2
@sweetlana <lana.grivnak@gmail.com>
2022-07-27 14:33:48 +0300

    add Calculator

--- spring-app2/src/main/java/ua/havryliv/springcourse/controllers/FirstController.java
+++ spring-app2/src/main/java/ua/havryliv/springcourse/controllers/FirstController.java
@@ -14,33 +14,64 @@ public class FirstController {
 
 	/*
 	 * @GetMapping("/hello") public String helloPage(HttpServletRequest request) {
-	 * String name = request.getParameter("name"); String surname =
-	 * request.getParameter("surname");
+	 * String name = request.getParameter("name"); 
+	 * String surname = request.getParameter("surname");
 	 * 
 	 * System.out.println("Hello," + name + " " + surname); return "first/hello"; }
 	 */
 
 	/*
-	 * @GetMapping("/hello") public String helloPage(@RequestParam("name") String
-	 * name, //expect that we populate params in url, if not - status 400
-	 * 
-	 * @RequestParam("surname") String surname) {
+	 * @GetMapping("/hello") public String helloPage(@RequestParam("name") String name, //expect that we populate params in url, if not - status 400
+	 * 												 @RequestParam("surname") String surname) {
 	 * 
 	 * System.out.println("Hello," + name + " " + surname); return "first/hello"; }
 	 */
 
-	@GetMapping("/hello") public String helloPage(@RequestParam(value = "name", required = false) String name,
+	@GetMapping("/hello") 
+	public String helloPage(@RequestParam(value = "name", required = false) String name,
 												  @RequestParam(value = "surname", required = false) String surname,
 												  Model model) {
 			
-			//System.out.println("Hello," + name + " " + surname); 
-			model.addAttribute("message", "Hello," + name + " " + surname);
+		//System.out.println("Hello," + name + " " + surname); 
+		model.addAttribute("message", "Hello," + name + " " + surname);
 		
-			return "first/hello"; }
+		return "first/hello"; }
 	
 
 	@GetMapping("/goodbye")
 	public String goodByePage() {
 		return "first/goodbye";
+	}
+	
+	@GetMapping("/calculator")
+	public String calculator(@RequestParam("a") int a, 
+							 @RequestParam("b") int b,
+							 @RequestParam("action") String action,
+							 Model model) {
+		String resultString = "Invalid parameters";
+		
+		if(action != null) {
+			switch(action) {
+			case "multiplication":
+				resultString =  a + " * " + b + " = " +  a*b;
+				break;
+			case "addition":
+				resultString = a + " + " + b + " = " +  (a+b);
+				break;
+			case "subtraction":
+				resultString = a + " - " + b + " = " + (a-b);
+				break;
+			case "division":
+				if(b != 0) {
+					resultString = a + " / " + b + " = " +  a/(double)b;
+				} else resultString = "Division by zero is deprecated";
+				break;
+			}
+		}
+		
+		model.addAttribute("result", resultString);
+		
+		return "first/calculator";
+		
 	}
 }
