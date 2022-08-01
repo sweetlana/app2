f906f101f709522bfc3d4cde0d72a53fcbeb3490
@sweetlana <lana.grivnak@gmail.com>
2022-08-02 00:47:48 +0300

    l24 add age and email

--- spring-app2/src/main/java/ua/havryliv/springcourse/dao/PersonDAO.java
+++ spring-app2/src/main/java/ua/havryliv/springcourse/dao/PersonDAO.java
@@ -15,10 +15,10 @@ public class PersonDAO {
 	{
 		people = new ArrayList<>();
 		
-		people.add(new Person(++PEOPLE_COUNT, "Tom"));
-		people.add(new Person(++PEOPLE_COUNT, "Bob"));
-		people.add(new Person(++PEOPLE_COUNT, "Mike"));
-		people.add(new Person(++PEOPLE_COUNT, "Katy"));
+		people.add(new Person(++PEOPLE_COUNT, "Tom", 19, "mail1@gmail.com"));
+		people.add(new Person(++PEOPLE_COUNT, "Bob", 20, "mail2@gmail.com"));
+		people.add(new Person(++PEOPLE_COUNT, "Mike", 21, "mail3@gmail.com"));
+		people.add(new Person(++PEOPLE_COUNT, "Katy", 22, "mail4@gmail.com"));
 	}
 	
 	public List<Person> index(){
@@ -38,6 +38,8 @@ public class PersonDAO {
 		Person personToBeUpdated = show(id);
 		
 		personToBeUpdated.setName(updatedPerson.getName());
+		personToBeUpdated.setAge(updatedPerson.getAge());
+		personToBeUpdated.setEmail(updatedPerson.getEmail());
 	}
 	
 	public void delete(int id) {
